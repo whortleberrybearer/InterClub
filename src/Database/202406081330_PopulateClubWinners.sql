dc5262d881d5a03ed158f1e0da8061bfd73de69e
whortleberrybearer <30476438+whortleberrybearer@users.noreply.github.com>
2024-06-08 20:33:30 +0100

    feat: Added historical winners (#6)

---
+++ src/Database/202406081330_PopulateClubWinners.sql
@@ -0,0 +1,623 @@
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1992 AND Competition = "Road"),
+    "Open",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1992 AND Competition = "Road"),
+    "Vet",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1993 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1993 AND Competition = "Road"),
+    "Female",
+    "Red Rose");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1993 AND Competition = "Road"),
+    "Vet",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1994 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1994 AND Competition = "Road"),
+    "Female",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1994 AND Competition = "Road"),
+    "Vet",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1995 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1995 AND Competition = "Road"),
+    "Female",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1995 AND Competition = "Road"),
+    "Vet",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1996 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1996 AND Competition = "Road"),
+    "Female",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1996 AND Competition = "Road"),
+    "Vet",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1997 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1997 AND Competition = "Road"),
+    "Female",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1997 AND Competition = "Road"),
+    "Vet",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1998 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1998 AND Competition = "Road"),
+    "Female",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1998 AND Competition = "Road"),
+    "Vet",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1999 AND Competition = "Road"),
+    "Open",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1999 AND Competition = "Road"),
+    "Female",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 1999 AND Competition = "Road"),
+    "Vet",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2000 AND Competition = "Road"),
+    "Open",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2000 AND Competition = "Road"),
+    "Female",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2000 AND Competition = "Road"),
+    "Vet",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2001 AND Competition = "Road"),
+    "Open",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2001 AND Competition = "Road"),
+    "Female",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2001 AND Competition = "Road"),
+    "Vet",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2002 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2002 AND Competition = "Road"),
+    "Female",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2002 AND Competition = "Road"),
+    "Vet",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2003 AND Competition = "Road"),
+    "Open",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2003 AND Competition = "Road"),
+    "Female",
+    "Blackpool and Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2003 AND Competition = "Road"),
+    "Vet",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2004 AND Competition = "Road"),
+    "Open",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2004 AND Competition = "Road"),
+    "Female",
+    "Blackpool");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2004 AND Competition = "Road"),
+    "Vet",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2005 AND Competition = "Road"),
+    "Open",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2005 AND Competition = "Road"),
+    "Female",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2005 AND Competition = "Road"),
+    "Vet",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2005 AND Competition = "Road"),
+    "Vet 50",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2006 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2006 AND Competition = "Road"),
+    "Female",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2006 AND Competition = "Road"),
+    "Vet",
+    "North Fylde");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2006 AND Competition = "Road"),
+    "Vet 50",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2006 AND Competition = "Road"),
+    "Vet 60",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2007 AND Competition = "Road"),
+    "Open",
+    "Blackpool");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2007 AND Competition = "Road"),
+    "Female",
+    "Blackpool");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2007 AND Competition = "Road"),
+    "Vet",
+    "Blackpool");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2007 AND Competition = "Road"),
+    "Vet 50",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2007 AND Competition = "Road"),
+    "Vet 60",
+    "Blackpool");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2008 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2008 AND Competition = "Road"),
+    "Female",
+    "Blackpool");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2008 AND Competition = "Road"),
+    "Vet",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2008 AND Competition = "Road"),
+    "Vet 50",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2008 AND Competition = "Road"),
+    "Vet 60",
+    "Blackpool");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2009 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2009 AND Competition = "Road"),
+    "Female",
+    "Blackpool");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2009 AND Competition = "Road"),
+    "Vet",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2009 AND Competition = "Road"),
+    "Vet 50",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2009 AND Competition = "Road"),
+    "Vet 60",
+    "Red Rose");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2010 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2010 AND Competition = "Road"),
+    "Female",
+    "Blackpool");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2010 AND Competition = "Road"),
+    "Vet",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2010 AND Competition = "Road"),
+    "Vet 50",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2010 AND Competition = "Road"),
+    "Vet 60",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2011 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2011 AND Competition = "Road"),
+    "Female",
+    "Blackpool");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2011 AND Competition = "Road"),
+    "Vet",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2011 AND Competition = "Road"),
+    "Vet 50",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2011 AND Competition = "Road"),
+    "Vet 60",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2012 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2012 AND Competition = "Road"),
+    "Female",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2012 AND Competition = "Road"),
+    "Vet",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2012 AND Competition = "Road"),
+    "Vet 50",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2012 AND Competition = "Road"),
+    "Vet 60",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2013 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2013 AND Competition = "Road"),
+    "Female",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2013 AND Competition = "Road"),
+    "Vet",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2013 AND Competition = "Road"),
+    "Vet 50",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2013 AND Competition = "Road"),
+    "Vet 60",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2014 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2014 AND Competition = "Road"),
+    "Female",
+    "Blackpool");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2014 AND Competition = "Road"),
+    "Vet",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2014 AND Competition = "Road"),
+    "Vet 50",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2014 AND Competition = "Road"),
+    "Vet 60",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2015 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2015 AND Competition = "Road"),
+    "Female",
+    "Blackpool");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2015 AND Competition = "Road"),
+    "Vet",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2015 AND Competition = "Road"),
+    "Vet 50",
+    "Red Rose");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2015 AND Competition = "Road"),
+    "Vet 60",
+    "Blackpool");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2016 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2016 AND Competition = "Road"),
+    "Female",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2016 AND Competition = "Road"),
+    "Vet",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2016 AND Competition = "Road"),
+    "Female Vet 40",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2016 AND Competition = "Road"),
+    "Vet 50",
+    "Red Rose");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2016 AND Competition = "Road"),
+    "Vet 60",
+    "Red Rpse");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2017 AND Competition = "Road"),
+    "Open",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2017 AND Competition = "Road"),
+    "Female",
+    "Lytham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2017 AND Competition = "Road"),
+    "Vet",
+    "Preston");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2017 AND Competition = "Road"),
+    "Female Vet 40",
+    "Wesham");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2017 AND Competition = "Road"),
+    "Vet 50",
+    "Red Rose");
+
+INSERT INTO ClubWinner (CompetitionId, Category, Club)
+VALUES (
+    (SELECT CompetitionId FROM Competition WHERE Year = 2017 AND Competition = "Road"),
+    "Vet 60",
+    "Red Rpse");
