231ee63a60b638cb9416de25f1eef558c09c05c0
Barry Wheeler <wheelerbarry@hotmail.com>
2024-06-18 11:54:00 +0100

    Updated club reuslts

--- src/Database/202406172100_RefactorCategories.sql
+++ src/Database/202406172100_RefactorCategories.sql
@@ -214,4 +214,84 @@ INNER JOIN YearClub yc
 ON yc.YearClubId = cw.YearClubId
 INNER JOIN Club cl
 ON cl.ClubId = yc.ClubId;
+
+DROP TABLE OldClubWinner;
+
+ALTER TABLE ClubResult
+RENAME TO OldClubResult;
+
+CREATE TABLE ClubResult (
+    ClubResultId INTEGER PRIMARY KEY AUTOINCREMENT,
+    RaceId INTEGER NOT NULL,
+    ClubCategoryId INTEGER NOT NULL,
+    YearClubId INTEGER NOT NULL, 
+    Position INTEGER NOT NULL,
+    Score INTEGER NOT NULL,
+    FOREIGN KEY (RaceId) REFERENCES Race (RaceId),
+    FOREIGN KEY (ClubCategoryId) REFERENCES ClubCategory (ClubCategoryId),
+    FOREIGN KEY (YearClubId) REFERENCES YearClub (YearClubId), 
+    UNIQUE(RaceId, ClubCategoryId, Position),
+    UNIQUE(RaceId, ClubCategoryId, YearClubId)
+);
+
+INSERT INTO ClubResult (
+    ClubResultId, 
+    RaceId, 
+    ClubCategoryId, 
+    YearClubId, 
+    Position, 
+    Score)
+SELECT 
+    ocr.ClubResultId,
+    ocr.RaceId,
+    cc.ClubCategoryId,
+    ocr.YearClubId,
+    ocr.Position,
+    ocr.Score
+FROM OldClubResult ocr
+INNER JOIN Race r
+ON r.RaceId = ocr.RaceId
+INNER JOIN ClubCategory cc
+ON cc.CompetitionId = r.CompetitionId
+INNER JOIN Category c
+ON c.CategoryId = cc.CategoryId
+AND c.Category = ocr.Category;
+
+DROP VIEW ClubResultsView;
+CREATE VIEW ClubResultsView
+AS
+SELECT 
+    cr.ClubResultId,
+    co.CompetitionId,
+    ct.CompetitionTypeId,
+    ct.CompetitionType,
+    y.YearId,
+    y.Year,
+    r.RaceId,
+    r.Name RaceName,
+    cc.ClubCategoryId,
+    ca.CategoryId,
+    ca.Category,
+    cr.Position,
+    yc.YearClubId,
+    cl.ClubId,
+    cl.ShortName ClubShortName,
+    cr.Score
+FROM ClubResult cr
+INNER JOIN Race r
+ON r.RaceId = cr.RaceId
+INNER JOIN Competition co
+ON co.CompetitionId = r.CompetitionId
+INNER JOIN CompetitionType ct
+ON ct.CompetitionTypeId = co.CompetitionTypeId
+INNER JOIN Year y
+ON y.YearId = co.YearId
+INNER JOIN YearClub yc
+ON yc.YearClubId = cr.YearClubId
+INNER JOIN Club cl
+ON cl.ClubId = yc.ClubId
+INNER JOIN ClubCategory cc
+ON cc.ClubCategoryId = cr.ClubCategoryId
+INNER JOIN Category ca
+ON ca.CategoryId = cc.CategoryId;
 */
