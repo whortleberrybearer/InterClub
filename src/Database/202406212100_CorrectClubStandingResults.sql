9a279dcbefd69684fc06bb8daeab1a83f82f534b
Barry Wheeler <wheelerbarry@hotmail.com>
2024-06-21 21:47:58 +0100

    Updated road 2024 standings

--- src/Database/202406212100_CorrectClubStandingResults.sql
+++ src/Database/202406212100_CorrectClubStandingResults.sql
@@ -5,27 +5,12 @@ WHERE ClubStandingResultId IN (
     LEFT OUTER JOIN ClubStanding cs
     ON cs.ClubStandingId = csr.ClubStandingId
     WHERE cs.ClubStandingId IS NULL);
-*/
-
-INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
-VALUES (
-    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Open" AND ClubShortName = "Preston"),
-    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
-    7
-);
 
 INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
 VALUES (
     (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Open" AND ClubShortName = "Blackpool"),
     (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
     5
-);
-
-INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
-VALUES (
-    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Open" AND ClubShortName = "Wesham"),
-    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
-    6
 );
 
 INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
@@ -44,6 +29,13 @@ VALUES (
 
 INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
 VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Open" AND ClubShortName = "Preston"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    7
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
     (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Open" AND ClubShortName = "Red Rose"),
     (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
     3
@@ -55,3 +47,256 @@ VALUES (
     (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
     1
 );
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Open" AND ClubShortName = "Wesham"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    6
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Female" AND ClubShortName = "Blackpool"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    6
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Female" AND ClubShortName = "Chorley"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    2
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Female" AND ClubShortName = "Lytham"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    5
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Female" AND ClubShortName = "Preston"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    7
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Female" AND ClubShortName = "Red Rose"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    4
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Female" AND ClubShortName = "Thornton"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    1
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Female" AND ClubShortName = "Wesham"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    3
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Female Vet 40" AND ClubShortName = "Blackpool"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    6
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Female Vet 40" AND ClubShortName = "Chorley"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    2
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Female Vet 40" AND ClubShortName = "Lytham"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    4
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Female Vet 40" AND ClubShortName = "Preston"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    5
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Female Vet 40" AND ClubShortName = "Red Rose"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    3
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Female Vet 40" AND ClubShortName = "Thornton"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    2
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Female Vet 40" AND ClubShortName = "Wesham"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    7
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet" AND ClubShortName = "Blackpool"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    5
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet" AND ClubShortName = "Chorley"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    1
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet" AND ClubShortName = "Lytham"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    3
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet" AND ClubShortName = "Preston"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    7
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet" AND ClubShortName = "Red Rose"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    4
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet" AND ClubShortName = "Thornton"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    2
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet" AND ClubShortName = "Wesham"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    6
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet 50" AND ClubShortName = "Blackpool"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    6
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet 50" AND ClubShortName = "Chorley"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    3
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet 50" AND ClubShortName = "Lytham"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    2
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet 50" AND ClubShortName = "Preston"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    7
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet 50" AND ClubShortName = "Red Rose"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    4
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet 50" AND ClubShortName = "Thornton"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    1
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet 50" AND ClubShortName = "Wesham"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    5
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet 60" AND ClubShortName = "Blackpool"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    5
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet 60" AND ClubShortName = "Chorley"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    2
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet 60" AND ClubShortName = "Lytham"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    4
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet 60" AND ClubShortName = "Preston"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    7
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet 60" AND ClubShortName = "Red Rose"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    6
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet 60" AND ClubShortName = "Thornton"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    1
+);
+
+INSERT INTO ClubStandingResult (ClubStandingId, RaceId, Points)
+VALUES (
+    (SELECT ClubStandingId FROM ClubStandingsView WHERE CompetitionType = "Road" AND Year = "2024" AND Category = "Vet 60" AND ClubShortName = "Wesham"),
+    (SELECT RaceId FROM RacesView WHERE CompetitionType = "Road" AND Year = "2024" AND Name = "Blackpool"),
+    3
+);
+*/
