2a9c52dbc32e5b564273cd0b17266cd59b207898
Barry Wheeler <wheelerbarry@hotmail.com>
2024-06-21 19:26:05 +0100

    Added club standngs template

---
+++ src/Website/src/templates/club-standings.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const ClubStandingsPage = () => {
+  return (
+    <main class="container">
+      <h1>Club Standings</h1>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
+
+export const Head = () => <title>About Me</title>
+
+export default ClubStandingsPage;
