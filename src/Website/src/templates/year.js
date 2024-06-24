81621c52c910d169cd8e87224e9c7057716e5d4d
Barry Wheeler <wheelerbarry@hotmail.com>
2024-06-24 07:14:30 +0100

    Added years template

--- src/Website/src/templates/year.js
+++ src/Website/src/templates/year.js
@@ -2,68 +2,22 @@ import * as React from 'react'
 import { graphql } from "gatsby"
 import Layout from '../organisms/layout'
 
-function findAndSortRunnerStandingsForCategory(runnerCategoryId, standings) {
-  return standings
-      .filter((s) => s.RunnerCategoryId === runnerCategoryId)
-      .sort((a, b) => a.Position - b.Position);
-}
-
-function findRunnerStandingResult(raceId, standingResults) { 
-  const runnerCategoryResult = standingResults.find((sr) => sr.RaceId === raceId);
-
-  return runnerCategoryResult?.Points;
-}
-
-
 const YearPage = ({data}) => {
-  const categoryOrder = [ "Open", "Male", "Female", "Male Vet 40", "Female Vet 40", "Male Vet 50", "Female Vet 50", "Male Vet 60", "Female Vet 60" ];
-  const runnerCategories = data.allSqliteRunnerCategories.nodes.sort((a, b) => {
-    return categoryOrder.indexOf(a.Category) - categoryOrder.indexOf(b.Category);
+  const competitionOrder = [ "Road", "Fell" ];
+  const competitions = data.allSqliteCompetitions.nodes.sort((a, b) => {
+    return competitionOrder.indexOf(a.CompetitionType) - competitionOrder.indexOf(b.CompetitionType);
   });
 
   return (
     <Layout>
       <main className="container">
-      <h1>{data.sqliteCompetitions.CompetitionType} Standings {data.sqliteCompetitions.Year}</h1>
-      
-      {runnerCategories.map((runnerCategory) => (
-        <div key={runnerCategory.RunnerCategoryId}>
-          <h2>{runnerCategory.Category}</h2>
+        <h1>{data.sqliteYears.Year}</h1>
 
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Category</th>
-                <th>Club</th>
-
-                {data.allSqliteRaces.nodes.map((race) => (
-                  <th key={race.RaceId}>{race.Name}</th>
-                ))}
-
-                <th>Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              {findAndSortRunnerStandingsForCategory(runnerCategory.RunnerCategoryId, data.allSqliteRunnerStandings.nodes).map((runnerStanding) => (
-                <tr key={runnerStanding.RunnerStandingId}>
-                  <td>{runnerStanding.Name} {runnerStanding.Surname}</td>
-                  <td>{runnerStanding.Sex}{runnerStanding.AgeCategory}</td>
-                  <td>{runnerStanding.ClubShortName}</td>
-
-                  {data.allSqliteRaces.nodes.map((race) => (
-                    <td key={`${runnerStanding.RunnerStandingId}-${race.RaceId}`}>
-                      {findRunnerStandingResult(race.RaceId, runnerStanding.RunnerStandingResults)}
-                    </td>
-                  ))}
-
-                  <td>{runnerStanding.Total}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      ))}
+        {competitions.map((competition) => (
+          <div key={competition.CompetitionId}>
+            <h2>{competition.CompetitionType}</h2>
+          </div>
+        ))} 
       </main>
     </Layout>
   )
@@ -72,55 +26,24 @@ const YearPage = ({data}) => {
 export const Head =  ({data}) => {
   return (
     <>
-      <title>{data.sqliteCompetitions.CompetitionType} Standings {data.sqliteCompetitions.Year}</title>
-      <meta name="description" content={`${data.sqliteCompetitions.CompetitionType} standings. Year: ${data.sqliteCompetitions.Year}.`} />
+      <title>{data.sqliteYears.Year}</title>
+      <meta name="description" content={`Inter Club details for ${data.sqliteYears.Year}.`} />
     </>
   );
 }
 
 export const query = graphql`
-  query Query($competitionId: Int) {
-    sqliteCompetitions(CompetitionId: {eq: $competitionId}) {
-      CompetitionId
-      CompetitionType
-      CompetitionTypeId
+  query Query($id: Int) {
+    sqliteYears(YearId: {eq: $id}) {
       Year
       YearId
     }
-    allSqliteRunnerCategories(filter: {CompetitionId: {eq: $competitionId}}) {
+    allSqliteCompetitions(filter: {YearId: {eq: $id}}) {
       nodes {
-        RunnerCategoryId
-        CategoryId
-        Category
+        CompetitionId
+        CompetitionType
       }
     }
-    allSqliteRaces(filter: {CompetitionId: {eq: $competitionId}}) {
-      nodes {
-        RaceId
-        Name
-        StartDateTime
-      }
-    }
-    allSqliteRunnerStandings(filter: {CompetitionId: {eq: $competitionId}}) {
-      nodes {
-        RunnerCategoryId
-        CategoryId
-        ClubId
-        ClubShortName
-        RunnerStandingId
-        Position
-        Name
-        Surname
-        Sex
-        AgeCategory
-        Total
-        RunnerStandingResults {
-          Points
-          RaceId
-          RunnerStandingResultId
-        }
-      }
-    }
-}`;
+  }`;
 
 export default YearPage;
