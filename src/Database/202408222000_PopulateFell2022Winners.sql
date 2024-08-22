1a793119333f980e92db8dd9ea1de719e8deb8d3
Barry Wheeler <wheelerbarry@hotmail.com>
2024-08-22 20:07:46 +0100

    Winners

--- src/Database/202408222000_PopulateFell2022Winners.sql
+++ src/Database/202408222000_PopulateFell2022Winners.sql
@@ -1,4 +1,4 @@
-INSERT INTO ClubWinner (ClubCategoryId, YearClubId)
+/*INSERT INTO ClubWinner (ClubCategoryId, YearClubId)
 VALUES (
     (SELECT ClubCategoryId FROM ClubCategoriesView WHERE Year = 2022 AND CompetitionType = "Fell" AND Category = "Open"),
     (SELECT ClubId FROM YearClubsView WHERE Year = 2022 AND ShortName = "Chorley"));
@@ -22,111 +22,123 @@ INSERT INTO ClubWinner (ClubCategoryId, YearClubId)
 VALUES (
     (SELECT ClubCategoryId FROM ClubCategoriesView WHERE Year = 2022 AND CompetitionType = "Fell" AND Category = "Vet 60"),
     (SELECT ClubId FROM YearClubsView WHERE Year = 2022 AND ShortName = "Chorley"));
-/*
-INSERT INTO RunnerWinner (CompetitionId, Category, Name, Surname, Club, Position)
+*/
+INSERT INTO RunnerWinner (RunnerCategoryId, Name, Surname, YearClubId, Position)
 VALUES 
-    ((SELECT CompetitionId FROM CompetitionsView WHERE Year = 2022 AND CompetitionType = "Fell"),
-    "Male",
-    "Alek",
-    "Walker",
-    "Wesham",
-    1);
-
-INSERT INTO RunnerWinner (CompetitionId, Category, Name, Surname, Club, Position)
-VALUES 
-    ((SELECT CompetitionId FROM CompetitionsView WHERE Year = 2022 AND CompetitionType = "Fell"),
-    "Male",
-    "Christopher",
-    "Bridge",
-    "Red Rose",
-    3);
-
-INSERT INTO RunnerWinner (CompetitionId, Category, Name, Surname, Club, Position)
-VALUES 
-    ((SELECT CompetitionId FROM CompetitionsView WHERE Year = 2022 AND CompetitionType = "Fell"),
-    "Male Vet 40",
-    "David",
-    "Cowburn",
-    "Chorley",
-    1);
-
-INSERT INTO RunnerWinner (CompetitionId, Category, Name, Surname, Club, Position)
-VALUES 
-    ((SELECT CompetitionId FROM CompetitionsView WHERE Year = 2022 AND CompetitionType = "Fell"),
-    "Male Vet 40",
-    "Steve",
-    "Myerscough",
-    "Wesham",
-    2);
-
-INSERT INTO RunnerWinner (CompetitionId, Category, Name, Surname, Club, Position)
-VALUES 
-    ((SELECT CompetitionId FROM CompetitionsView WHERE Year = 2022 AND CompetitionType = "Fell"),
-    "Male Vet 50",
+    ((SELECT RunnerCategoryId FROM RunnerCategoriesView WHERE Year = 2022 AND CompetitionType = "Fell" AND Category = "Male"),
     "Darren",
     "McDermott",
-    "Preston",
+    (SELECT ClubId FROM YearClubsView WHERE Year = 2022 AND ShortName = "Preston"),
     1);
 
-INSERT INTO RunnerWinner (CompetitionId, Category, Name, Surname, Club, Position)
+INSERT INTO RunnerWinner (RunnerCategoryId, Name, Surname, YearClubId, Position)
 VALUES 
-    ((SELECT CompetitionId FROM CompetitionsView WHERE Year = 2022 AND CompetitionType = "Fell"),
-    "Male Vet 50",
-    "Carl",
-    "Groome",
-    "Wesham",
-    2);
-
-INSERT INTO RunnerWinner (CompetitionId, Category, Name, Surname, Club, Position)
-VALUES 
-    ((SELECT CompetitionId FROM CompetitionsView WHERE Year = 2022 AND CompetitionType = "Fell"),
-    "Male Vet 60",
+    ((SELECT RunnerCategoryId FROM RunnerCategoriesView WHERE Year = 2022 AND CompetitionType = "Fell" AND Category = "Male"),
     "Steve",
     "Baker",
-    "Chorley",
-    1);
-
-INSERT INTO RunnerWinner (CompetitionId, Category, Name, Surname, Club, Position)
-VALUES 
-    ((SELECT CompetitionId FROM CompetitionsView WHERE Year = 2022 AND CompetitionType = "Fell"),
-    "Male Vet 60",
-    "Philip",
-    "Butler",
-    "Red Rose",
+    (SELECT ClubId FROM YearClubsView WHERE Year = 2022 AND ShortName = "Chorley"),
     2);
 
-INSERT INTO RunnerWinner (CompetitionId, Category, Name, Surname, Club, Position)
+INSERT INTO RunnerWinner (RunnerCategoryId, Name, Surname, YearClubId, Position)
 VALUES 
-    ((SELECT CompetitionId FROM CompetitionsView WHERE Year = 2022 AND CompetitionType = "Fell"),
-    "Female",
-    "Kay",
-    "Twist",
-    "Wesham",
-    1);
-
-INSERT INTO RunnerWinner (CompetitionId, Category, Name, Surname, Club, Position)
-VALUES 
-    ((SELECT CompetitionId FROM CompetitionsView WHERE Year = 2022 AND CompetitionType = "Fell"),
-    "Female",
-    "Julia",
-    "Rolfe",
-    "Lytham",
+    ((SELECT RunnerCategoryId FROM RunnerCategoriesView WHERE Year = 2022 AND CompetitionType = "Fell" AND Category = "Male"),
+    "Paul",
+    "Lancashire",
+    (SELECT ClubId FROM YearClubsView WHERE Year = 2022 AND ShortName = "Wesham"),
     3);
 
-INSERT INTO RunnerWinner (CompetitionId, Category, Name, Surname, Club, Position)
+INSERT INTO RunnerWinner (RunnerCategoryId, Name, Surname, YearClubId, Position)
 VALUES 
-    ((SELECT CompetitionId FROM CompetitionsView WHERE Year = 2022 AND CompetitionType = "Fell"),
-    "Female Vet 40",
-    "Olga",
-    "Wiggins",
-    "Preston",
+    ((SELECT RunnerCategoryId FROM RunnerCategoriesView WHERE Year = 2022 AND CompetitionType = "Fell" AND Category = "Male Vet 40"),
+    "John",
+    "Naylor",
+    (SELECT ClubId FROM YearClubsView WHERE Year = 2022 AND ShortName = "Red Rose"),
     1);
 
-INSERT INTO RunnerWinner (CompetitionId, Category, Name, Surname, Club, Position)
+INSERT INTO RunnerWinner (RunnerCategoryId, Name, Surname, YearClubId, Position)
 VALUES 
-    ((SELECT CompetitionId FROM CompetitionsView WHERE Year = 2022 AND CompetitionType = "Fell"),
-    "Female Vet 50",
+    ((SELECT RunnerCategoryId FROM RunnerCategoriesView WHERE Year = 2022 AND CompetitionType = "Fell" AND Category = "Male Vet 40"),
+    "Paul",
+    "Lancashire",
+    (SELECT ClubId FROM YearClubsView WHERE Year = 2022 AND ShortName = "Wesham"),
+    2);
+
+INSERT INTO RunnerWinner (RunnerCategoryId, Name, Surname, YearClubId, Position)
+VALUES 
+    ((SELECT RunnerCategoryId FROM RunnerCategoriesView WHERE Year = 2022 AND CompetitionType = "Fell" AND Category = "Male Vet 50"),
+    "Darren",
+    "McDermott",
+    (SELECT ClubId FROM YearClubsView WHERE Year = 2022 AND ShortName = "Preston"),
+    1);
+
+INSERT INTO RunnerWinner (RunnerCategoryId, Name, Surname, YearClubId, Position)
+VALUES 
+    ((SELECT RunnerCategoryId FROM RunnerCategoriesView WHERE Year = 2022 AND CompetitionType = "Fell" AND Category = "Male Vet 50"),
+    "Mark",
+    "Ellithorn",
+    (SELECT ClubId FROM YearClubsView WHERE Year = 2022 AND ShortName = "Chorley"),
+    2);
+
+INSERT INTO RunnerWinner (RunnerCategoryId, Name, Surname, YearClubId, Position)
+VALUES 
+    ((SELECT RunnerCategoryId FROM RunnerCategoriesView WHERE Year = 2022 AND CompetitionType = "Fell" AND Category = "Male Vet 60"),
+    "Steve",
+    "Baker",
+    (SELECT ClubId FROM YearClubsView WHERE Year = 2022 AND ShortName = "Chorley"),
+    1);
+
+INSERT INTO RunnerWinner (RunnerCategoryId, Name, Surname, YearClubId, Position)
+VALUES 
+    ((SELECT RunnerCategoryId FROM RunnerCategoriesView WHERE Year = 2022 AND CompetitionType = "Fell" AND Category = "Female"),
     "Claire",
     "McDermott",
-    "Preston",
-    1);*/
+    (SELECT ClubId FROM YearClubsView WHERE Year = 2022 AND ShortName = "Preston"),
+    1);
+
+INSERT INTO RunnerWinner (RunnerCategoryId, Name, Surname, YearClubId, Position)
+VALUES 
+    ((SELECT RunnerCategoryId FROM RunnerCategoriesView WHERE Year = 2022 AND CompetitionType = "Fell" AND Category = "Female"),
+    "Claire",
+    "Markham",
+    (SELECT ClubId FROM YearClubsView WHERE Year = 2022 AND ShortName = "Lytham"),
+    2);
+
+INSERT INTO RunnerWinner (RunnerCategoryId, Name, Surname, YearClubId, Position)
+VALUES 
+    ((SELECT RunnerCategoryId FROM RunnerCategoriesView WHERE Year = 2022 AND CompetitionType = "Fell" AND Category = "Female"),
+    "Amanda",
+    "Fuller",
+    (SELECT ClubId FROM YearClubsView WHERE Year = 2022 AND ShortName = "Blackpool"),
+    3);
+
+INSERT INTO RunnerWinner (RunnerCategoryId, Name, Surname, YearClubId, Position)
+VALUES 
+    ((SELECT RunnerCategoryId FROM RunnerCategoriesView WHERE Year = 2022 AND CompetitionType = "Fell" AND Category = "Female Vet 40"),
+    "Claire",
+    "Markham",
+    (SELECT ClubId FROM YearClubsView WHERE Year = 2022 AND ShortName = "Lytham"),
+    1);
+
+INSERT INTO RunnerWinner (RunnerCategoryId, Name, Surname, YearClubId, Position)
+VALUES 
+    ((SELECT RunnerCategoryId FROM RunnerCategoriesView WHERE Year = 2022 AND CompetitionType = "Fell" AND Category = "Female Vet 40"),
+    "Olga",
+    "Wiggins",
+    (SELECT ClubId FROM YearClubsView WHERE Year = 2022 AND ShortName = "Preston"),
+    2);
+
+INSERT INTO RunnerWinner (RunnerCategoryId, Name, Surname, YearClubId, Position)
+VALUES 
+    ((SELECT RunnerCategoryId FROM RunnerCategoriesView WHERE Year = 2022 AND CompetitionType = "Fell" AND Category = "Female Vet 50"),
+    "Claire",
+    "McDermott",
+    (SELECT ClubId FROM YearClubsView WHERE Year = 2022 AND ShortName = "Preston"),
+    1);
+
+INSERT INTO RunnerWinner (RunnerCategoryId, Name, Surname, YearClubId, Position)
+VALUES 
+    ((SELECT RunnerCategoryId FROM RunnerCategoriesView WHERE Year = 2022 AND CompetitionType = "Fell" AND Category = "Female Vet 50"),
+    "Pamela",
+    "Hardman",
+    (SELECT ClubId FROM YearClubsView WHERE Year = 2022 AND ShortName = "Lytham"),
+    2);
