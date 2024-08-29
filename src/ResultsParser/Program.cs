06cfe3480cb3f787ccd2eafe0f454c674259ac13
Barry Wheeler <wheelerbarry@hotmail.com>
2024-08-29 21:29:25 +0100

    Fixed null

--- src/ResultsParser/Program.cs
+++ src/ResultsParser/Program.cs
@@ -156,7 +156,7 @@ void SaveClubResults(
                     new
                     {
                         clubResultId = result.ClubResultId,
-                        raceResultId = teamScorer.Result.RaceResultId,
+                        raceResultId = teamScorer.Result!.RaceResultId,
                     },
                     transaction);
             }
